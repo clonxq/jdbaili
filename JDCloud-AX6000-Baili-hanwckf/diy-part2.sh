7378cd8bff29a7124fcbffb050ff3ac517aac995
xqrock <58764635+xqrock@users.noreply.github.com>
2025-09-29 21:24:26 +0800

    Update diy-part2.sh

--- JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
+++ JDCloud-AX6000-Baili-hanwckf/diy-part2.sh
@@ -71,4 +71,4 @@ rm -rf ./feeds/luci/applications/luci-app-wechatpush
 rm -rf ./feeds/luci/applications/luci-app-ddns-go
 rm -rf ./feeds/luci/applications/luci-app-lucky
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
