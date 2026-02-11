70f2e461d345cefd468cc43f325ea655cb913f33
xqrock <58764635+xqrock@users.noreply.github.com>
2026-02-11 13:20:07 +0800

    Update padavanonlypart2.sh

--- padavanonlypart2.sh
+++ padavanonlypart2.sh
@@ -29,4 +29,12 @@ rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
-sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+#修复Rust编译失败
+RUST_FILE=$(find ./feeds/packages/ -maxdepth 3 -type f -wholename "*/rust/Makefile")
+if [ -f "$RUST_FILE" ]; then
+	echo " "
+
+	sed -i 's/ci-llvm=true/ci-llvm=false/g' $RUST_FILE
+
+	cd $PKG_PATH && echo "rust has been fixed!"
+fi
