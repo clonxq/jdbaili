5d06c5039f61e7680b9f4acac51aa54071e83e46
xqrock <58764635+xqrock@users.noreply.github.com>
2026-02-12 14:45:55 +0800

    Update ledepart2.sh

--- ledepart2.sh
+++ ledepart2.sh
@@ -10,16 +10,24 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#修改默认IP地址
+sed -i 's/192\.168\.[0-9]*\.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 ##-----------------Del duplicate packages------------------
-rm -rf feeds/packages/net/open-app-filter
+rm -rf ./feeds/packages/net/adguardhome
+rm -rf ./feeds/packages/net/lucky
+rm -rf ./feeds/packages/net/mosdns
+rm -rf ./feeds/packages/net/nikki
+rm -rf ./feeds/packages/net/smartdns
+rm -rf ./feeds/luci/applications/luci-app-adguardhome
+rm -rf ./feeds/luci/applications/luci-app-dockerman
+rm -rf ./feeds/luci/applications/luci-app-homeproxy
+rm -rf ./feeds/luci/applications/luci-app-lucky
+rm -rf ./feeds/luci/applications/luci-app-mosdns
+rm -rf ./feeds/luci/applications/luci-app-nikki
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-passwall2
+rm -rf ./feeds/luci/applications/luci-app-openclash
+rm -rf ./feeds/luci/applications/luci-app-smartdns
+rm -rf ./feeds/luci/applications/luci-app-ssr-plus
 ##-----------------DIY-----------------
-# rm -rf ./feeds/packages/net/adguardhome
-# rm -rf ./feeds/packages/net/mosdns
-# rm -rf ./feeds/packages/net/shadowsocks-libev
-# rm -rf ./feeds/packages/net/shadowsocks-rust
-# rm -rf ./feeds/packages/net/shadowsocksr-libev
-# rm -rf ./feeds/luci/applications/luci-app-passwall
-# rm -rf ./feeds/luci/applications/luci-app-passwall2
-# rm -rf ./feeds/luci/applications/luci-app-ssr-plus
+
