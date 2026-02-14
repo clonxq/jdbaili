13fb6ca9608b579ac96fb8f62475e5a13fee20ed
xqrock <58764635+xqrock@users.noreply.github.com>
2026-02-14 20:52:27 +0800

    Update padavanonlypart1.sh

--- padavanonlypart1.sh
+++ padavanonlypart1.sh
@@ -8,35 +8,46 @@
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-function merge_package(){
-    repo=`echo $1 | rev | cut -d'/' -f 1 | rev`
-    pkg=`echo $2 | rev | cut -d'/' -f 1 | rev`
-    # find package/ -follow -name $pkg -not -path "package/custom/*" | xargs -rt rm -rf
-    git clone --depth=1 --single-branch $1
-    mv $2 package/custom/
-    rm -rf $repo
+# --- 定义通用局部拉取函数 ---
+function extract_pkg() {
+    local repo_url=$1
+    local repo_path=$2
+    local local_path=$3
+    local branch=${4:-""}
+    local tmp_dir="tmp_extract_$(date +%s)"
+
+    echo ">>> 正在提取: $repo_path (分支: ${branch:-默认})"
+    local clone_args="--depth 1 --filter=blob:none --sparse"
+    [ -n "$branch" ] && clone_args="$clone_args -b $branch"
+
+    git clone $clone_args "$repo_url" "$tmp_dir"
+    if [ $? -eq 0 ]; then
+        cd "$tmp_dir" || return
+        git sparse-checkout set "$repo_path"
+        cd ..
+        mkdir -p "$(dirname "$local_path")"
+        [ -d "$local_path" ] && rm -rf "$local_path"
+        if [ -d "$tmp_dir/$repo_path" ]; then
+            cp -r "$tmp_dir/$repo_path" "$local_path"
+            echo ">>> 成功保存至: $local_path"
+        fi
+        rm -rf "$tmp_dir"
+    fi
 }
-function drop_package(){
-    find package/ -follow -name $1 -not -path "package/custom/*" | xargs -rt rm -rf
-}
 
+# --- 插件拉取列表 (全部存放至 package/diy) ---
 
-# Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/OpenWrt-nikki
-git clone -b js github.com/0x676e67/luci-theme-design.git package/luci-theme-design
-# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
-git clone https://github.com/sirpdboy/luci-app-advanced.git package/luci-app-advanced
-# git clone https://github.com/KFERMercer/luci-app-tcpdump.git package/luci-app-tcpdump
-# git clone https://github.com/linkease/istore.git package/istore
-# git clone https://github.com/linkease/istore-ui.git package/istore-ui
-find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
-find ./ | grep Makefile | grep mosdns | xargs rm -f
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
-git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
-git clone -b js https://github.com/sirpdboy/luci-app-adguardhome package/luci-app-adguardhome
+# 1. 提取 nikki (从一个仓库提取两个目录)
+extract_pkg "https://github.com/nikkinikki-org/OpenWrt-nikki.git" "luci-app-nikki" "package/diy/luci-app-nikki"
+extract_pkg "https://github.com/nikkinikki-org/OpenWrt-nikki.git" "nikki" "package/diy/nikki"
+
+# 2. 其他插件 (指定分支或默认)
+extract_pkg "https://github.com/0x676e67/luci-theme-design.git" "luci-theme-design" "package/diy/luci-theme-design" "js"
+extract_pkg "https://github.com/sirpdboy/luci-app-advanced.git" "luci-app-advanced" "package/diy/luci-app-advanced"
+extract_pkg "https://github.com/sbwml/luci-app-mosdns.git" "luci-app-mosdns" "package/diy/mosdns" "v5"
+extract_pkg "https://github.com/sbwml/v2ray-geodata.git" "v2ray-geodata" "package/diy/v2ray-geodata"
+extract_pkg "https://github.com/sirpdboy/luci-app-adguardhome.git" "luci-app-adguardhome" "package/diy/luci-app-adguardhome-sirp" "js"
+
+# 3. 预清理 Makefile (防止源码自带的旧版干扰)
+find ./ -name Makefile | grep -E "v2ray-geodata|mosdns" | xargs -r rm -f
